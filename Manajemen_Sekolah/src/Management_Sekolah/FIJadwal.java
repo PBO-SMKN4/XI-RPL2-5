4dae4d71c8b6c2db3115f01cbf32fa33431e929a
satriaaprr99 <satriaaprr99@gmail.com>
2020-05-02 13:45:13 +0700

    refisi gercep 2

--- Manajemen_Sekolah/src/Management_Sekolah/FIJadwal.java
+++ Manajemen_Sekolah/src/Management_Sekolah/FIJadwal.java
@@ -5,6 +5,9 @@
  */
 package Management_Sekolah;
 
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
 import javax.swing.plaf.basic.BasicInternalFrameUI;
 
 /**
@@ -13,37 +16,60 @@ import javax.swing.plaf.basic.BasicInternalFrameUI;
  */
 public class FIJadwal extends javax.swing.JInternalFrame {
 
-    /**
-     * Creates new form FIJadwal
-     */
     public FIJadwal() {
         initComponents();
         
         this.setBorder(javax.swing.BorderFactory.createEmptyBorder(0, 0, 0, 0));
         BasicInternalFrameUI bi = (BasicInternalFrameUI)this.getUI();
         bi.setNorthPane(null);
+       
+        
     }
 
+    public static BufferedImage resize(BufferedImage img, int 
+newW, int newH) { 
+    int w = img.getWidth(); 
+            int h = img.getHeight(); 
+            BufferedImage dimg = dimg = new BufferedImage(newW, newH, 
+img.getType()); 
+            Graphics2D g = dimg.createGraphics(); 
+            g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, 
+RenderingHints.VALUE_INTERPOLATION_BILINEAR); 
+            g.drawImage(img, 0, 0, newW, newH, 0, 0, w, h, null); 
+            g.dispose(); 
+            return dimg; 
+        }
     
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
+        img = new javax.swing.JLabel();
 
         setPreferredSize(new java.awt.Dimension(840, 520));
 
         jPanel1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0), 5));
 
+        img.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/Screenshot (33).png"))); // NOI18N
+        img.setText("jLabel1");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 895, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(img)
+                .addContainerGap(387, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 498, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(img)
+                .addContainerGap(215, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -62,6 +88,7 @@ public class FIJadwal extends javax.swing.JInternalFrame {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel img;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
